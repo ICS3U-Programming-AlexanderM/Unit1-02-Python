6819e7df37e439074815f3ae2f786460dec04dcc
Ubuntu <ubuntu@ip-172-31-40-13.ec2.internal>
2021-11-25 18:12:51 +0000

    Added hello_world_updated.py

---
+++ hello_world_updated.py
@@ -0,0 +1,15 @@
+# Created by Alexander Matheson
+# Created on November 25 2021
+# This program is the hello_world_updated.py file
+
+
+def main():
+    print("Hello, World!")
+    print("By: Alexander Matheson")
+    print("Can't wait to get started.")
+    print("Hello, Daniel I.")
+    print("I was away when I was supposed to make this file.")
+
+
+if __name__ == "__main__":
+    main()
